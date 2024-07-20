a22c61a35202627686e2a2d167cbd712a88506c8
valentina.pera2007@gmail.com <valentina.pera2007@gmail.com>
2024-07-20 08:15:56 -0300

    integração das apis cores e marcas e telas

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,9 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import CategoriaView from "../views/CategoriaView.vue";
 import LoginView from "../views/LoginView.vue";
-import AcessorioView from "..views/AcessorioView.vue";
+import AcessorioView from "../views/AcessorioView.vue";
+import MarcaView from "../views/MarcaView.vue";
+import CorView from "../views/CorView.vue";
 import LogoutView from "../views/LogoutView.vue";
 
 const router = createRouter({
@@ -22,6 +24,16 @@ const router = createRouter({
       path: "/acessorios",
       name: "acessorios",
       component: AcessorioView,
+    },
+    {
+      path: "/cores",
+      name: "cores",
+      component: CorView,
+    },
+    {
+      path: "/marcas",
+      name: "marcas",
+      component: MarcaView,
     },
     {
       path: "/login",
